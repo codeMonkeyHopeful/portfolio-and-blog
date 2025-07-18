7351499a8a377d4dac5cafa0d3e474d1b1bb4fbb
Ryan Jasinski <jasinski.17@gmail.com>
2025-07-18 00:56:45 -0700

    bootstrap-js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// Enable bootstrap JS
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
