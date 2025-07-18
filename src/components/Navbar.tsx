1addd459ae7dfc248b215c8040df6d361589facd
Ryan Jasinski <jasinski.17@gmail.com>
2025-07-18 01:40:19 -0700

    links fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,45 @@
 import { FC } from "react";
+import Link from "next/link";
+
+const navLinks = [{}];
 
 export const Navbar: FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
+        <Link
+          href="/"
+          className="nav-link active navbar-brand"
+          aria-current="page"
+        >
+          {"My Coding Journey"}
+        </Link>
+
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
           aria-controls="navbarNav"
-          aria-expanded="false"
+          aria-expanded="true"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon">asdf</span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
+              <Link href="/" className="nav-link active" aria-current="page">
+                {"Home"}
+              </Link>
             </li>
+            <Link href="/" className="nav-link active" aria-current="page">
+              {"Projects"}
+            </Link>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
+              <Link href="/" className="nav-link active" aria-current="page">
+                {"Job Search Blog"}
+              </Link>
             </li>
           </ul>
         </div>
