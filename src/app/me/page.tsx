ae617240b16d974de03361e5e3aa402dc6fd0cd6
Ryan Jasinski <jasinski.17@gmail.com>
2025-07-20 20:38:40 -0700

    lay out topics for me page

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,12 +1,27 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "About Me",
-  description: "Learn more about codeMonkeyHopeful.",
+  title: 'About Me',
+  description: 'Learn more about codeMonkeyHopeful.',
 };
 
 const Me = ({}) => {
-  return <div>Info about myself</div>;
+  // TODO Fill in
+  return (
+    <div>
+      <h3>
+        Topics
+        <small className="text-muted">WIP</small>
+      </h3>
+      <ul className="list-group">
+        <li className="list-group-item">first pc/tech</li>
+        <li className="list-group-item">entry into coding</li>
+        <li className="list-group-item">hobbies</li>
+        <li className="list-group-item">school?</li>
+        <li className="list-group-item">kiwi and vicky</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Me;
