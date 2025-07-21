d6a64fbfa75a488dd46525eba9408b61a159481b
Ryan Jasinski <jasinski.17@gmail.com>
2025-07-20 20:20:32 -0700

    route created

---
+++ src/app/me/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Me",
+  description: "Learn more about codeMonkeyHopeful.",
+};
+
+const Me = ({}) => {
+  return <div>Info about myself</div>;
+};
+
+export default Me;
