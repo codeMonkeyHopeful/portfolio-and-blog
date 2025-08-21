736ecc76eff0a6ab0508f5064556c20e59120909
Ryan Jasinski <jasinski.17@gmail.com>
2025-08-21 05:19:00 -0700

    start table create links and display data

--- src/components/Projects/ProjectsList.tsx
+++ src/components/Projects/ProjectsList.tsx
@@ -2,5 +2,36 @@ import { projectLinkPlaceholder } from '@/constants/index';
 import { FC } from 'react';
 
 export const ProjectsList: FC = ({}) => {
-  return <div>temp</div>;
+  return (
+    <div className="container-md">
+      <div className="card">
+        <div className="card-body text-center">
+          <h5 className="card-title">Projects</h5>
+        </div>
+        <table className="table">
+          <thead>
+            <th scope="col">Project Name</th>
+            <th scope="col">Link</th>
+            <th scope="col">Why Did I Make It?</th>
+          </thead>
+          <tbody>
+            {projectLinkPlaceholder.map((el) => {
+              const formattedLink = (
+                <a href={el.url} target="_blank">
+                  Repo
+                </a>
+              );
+              return (
+                <tr key={el.url}>
+                  <td>{el.name}</td>
+                  <td>{formattedLink}</td>
+                  <td>{el.purpose}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
