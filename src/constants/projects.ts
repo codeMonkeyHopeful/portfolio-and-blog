903f472bd7341bef09a54f1329a482b013d22638
Ryan Jasinski <jasinski.17@gmail.com>
2025-08-21 04:54:07 -0700

    var filled in

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,1 +1,42 @@
-export const projectLinkPlaceholder = [{}, {}];
+export const projectLinkPlaceholder = [
+  {
+    name: 'portfolio-and-blog',
+    url: 'https://github.com/codeMonkeyHopeful/portfolio-and-blog',
+    stack: [''],
+    purpose: 'This project!',
+  },
+  {
+    name: 'shopping-app',
+    url: 'https://github.com/codeMonkeyHopeful/shopping-app',
+    stack: [''],
+    purpose:
+      'I wanted something that was live, on my phone, and my wife could also easily use. When we think of something we need from the store, it takes two seconds to add.  When we go to the store, there it is on our phone, checking items off as we go.',
+  },
+  {
+    name: 'movie-sync',
+    url: 'https://github.com/codeMonkeyHopeful/movie-sync',
+    stack: [''],
+    purpose:
+      'Running a home server and migrating files over I got tired of depending on 3rd party programs.  This uses native linux functionality, an easy to use CLI, and can both push and pull from the server.  It also includes de-duping and formatting.',
+  },
+  {
+    name: 'generic-af-pg',
+    url: 'https://github.com/codeMonkeyHopeful/generic-af-pg',
+    stack: [''],
+    purpose:
+      'A less technical friend was trying to get a Docker container up for a side project. I threw this together so he would have a solid starting point and have a UI to access the DB.',
+  },
+  {
+    name: 'alerts',
+    url: 'https://github.com/codeMonkeyHopeful/alerts',
+    stack: [''],
+    purpose:
+      'A half finished repo that was migrated directly into other projects as a module.  Using some credentials on the user side this sends a dynamic email out to whoever you want.  Great for tracking triggers in an API.',
+  },
+  {
+    name: 'EZLinkZ',
+    url: 'https://github.com/codeMonkeyHopeful/EZLinkZ',
+    stack: [''],
+    purpose: `Fun project to that copies highlighted link's href instead of the text.  I made this to better understand TamperMonkey but also to grab link content for another project in a much faster fashion.`,
+  },
+];
