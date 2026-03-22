e0aa5ee5ee5ad5ab9356c3688e96b284a56449f2
Ryan Jasinski <jasinski.17@gmail.com>
2026-03-21 17:51:59 -0700

    add constants for about me sections

---
+++ src/constants/aboutMe.ts
@@ -0,0 +1,27 @@
+export const aboutMeSections = [
+  {
+    title: 'codeMonkeyHopeful?!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.',
+  },
+  {
+    title: 'My Support System',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.',
+  },
+  {
+    title: 'Entry Into Tech',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.',
+  },
+  {
+    title: 'Hobbies and Interests',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.',
+  },
+  {
+    title: 'School IS Cool',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.',
+  },
+];
